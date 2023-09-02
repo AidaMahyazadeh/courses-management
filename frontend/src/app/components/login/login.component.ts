71c7c0e212f1dd67d3461aa706837b9e94b6a5b8
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-09-02 17:13:45 +0200

    created login form.

--- frontend/src/app/components/login/login.component.ts
+++ frontend/src/app/components/login/login.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ILogin } from 'src/app/core/models/login.model';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  loginForm !:FormGroup;
+  eyeIcon = 'fa-eye-slash';
+  type = 'password';
+  isText = false;
 
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      usernam : new FormControl(''),
+      password : new FormControl('')
+    })
+  }
+
+  hideShowPassword(){
+    this.isText =!this.isText
+    this.type = this.isText ?'text':'password';
+    this.eyeIcon = this.isText ?'fa-eye': 'fa-eye-slash';
+  }
+  
+onLogin(){
+
+}
 }
