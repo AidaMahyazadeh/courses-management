8315c2445b87c34cd60f0a50207b79a25131e134
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-09-04 12:02:49 +0200

    created signup model.

---
+++ frontend/src/app/core/models/signup.model.ts
@@ -0,0 +1,8 @@
+export default interface ISignUpForm {
+    firstName : string ;
+    lastName :string ;
+    userName : string ;
+    email : string ;
+    password :string ;
+    role ?: string ;
+}
