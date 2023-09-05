49d4b157ef2c0c6b9bef3db5206a8ee8e4e8da7f
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-09-05 18:32:22 +0200

    shown username after login.

---
+++ frontend/src/app/components/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { AuthStorageService } from 'src/app/core/services/auth-storage.service';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent implements OnInit{
+userName !:string;
+screenImage ='../../../assets/screenImage/shutterstock_1612044622.jpg'
+
+constructor(private authStorageService :AuthStorageService){}
+
+ngOnInit(){
+ return this.userName = this.authStorageService.getUserName()!
+}
+
+}
