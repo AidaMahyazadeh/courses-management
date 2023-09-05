51d73aee5d7afbc666bbc0a3d69966b3aaf2b81c
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-09-05 18:19:59 +0200

    imported some modules after installing ngx-toastr.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,23 +3,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule} from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ToastrModule } from 'ngx-toastr';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { SignupComponent } from './components/signup/signup.component';
+import { CoreModule } from './core/core.module';
+import { HeaderComponent } from './core/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot(),
+    CoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
