80821ea6689165ef5b31192789411b4093a3c333
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-09-05 18:35:05 +0200

    inject ToastrService.

--- frontend/src/app/components/signup/signup.component.ts
+++ frontend/src/app/components/signup/signup.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
+import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
+import { AuthenticationService } from 'src/app/core/services/authentication.service';
+
 @Component({
   selector: 'app-signup',
   templateUrl: './signup.component.html',
@@ -14,12 +18,18 @@ export class SignupComponent implements OnInit {
   passwordPattern = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,10}$/;
    emailPattern =/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
   
+  constructor(
+    private authService:AuthenticationService,
+    private toast :ToastrService,
+    private router :Router
+  ){}
+
 
   ngOnInit(): void {
     this.signupForm = new FormGroup({
-      firstName : new FormControl('',Validators.required),
-      lastName : new FormControl('',Validators.required),
-      userName : new FormControl('',[Validators.required,Validators.minLength(4)]),
+      firstname : new FormControl('',Validators.required),
+      lastname : new FormControl('',Validators.required),
+      username : new FormControl('',[Validators.required,Validators.minLength(4)]),
       email : new FormControl('',[Validators.required,Validators.email,Validators.pattern(this.emailPattern)]),
       password : new FormControl('',[Validators.required,Validators.pattern(this.passwordPattern)])
     })
@@ -33,9 +43,19 @@ export class SignupComponent implements OnInit {
 
   onSignup(){
     if(this.signupForm.valid){
-
+      this.authService.signup(this.signupForm.value).subscribe({
+        next :(res =>{
+          console.log(res)
+          this.toast.success(`${this.signupForm.controls['firstname'].value}, you are signed up successfully.`)
+          this.signupForm.reset();
+          this.router.navigate(['/login'])
+        }),
+        error :(err=>{
+          this.toast.error(err.error.message)
+        })
+      })
     }else{
-
+       this.toast.warning('You should fill in all fields.')
     }
   }
 }
