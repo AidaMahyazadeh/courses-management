206626091420bc7cf99ca60f01ce2a43dd3fc0bd
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-09-06 11:41:17 +0200

    imported CoursesRoutingModule.

---
+++ frontend/src/app/core/models/course.model.ts
@@ -0,0 +1,11 @@
+export default interface ICourse {
+    id :number;
+    name:string;
+    author:string;
+    duration :number;
+    type : string;
+    price :number;
+    image :string;
+    rating : number;
+    description :string;
+}
