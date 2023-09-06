5d4323d741609f93772626c78d2a6cad4e126642
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-09-06 10:29:51 +0200

    put admin$courses module like lazy module.

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './components/login/login.component';
-import { SignupComponent } from './components/signup/signup.component';
-import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './shared/components/login/login.component';
+import { SignupComponent } from './shared/components/signup/signup.component';
+import { HomeComponent } from './shared/components/home/home.component';
+import { adminAuthGuard } from './core/services/guards/admin-auth.guard';
 
 const routes: Routes = [
   {path : '', redirectTo :'login',pathMatch:'full'},
   {path : 'home', component: HomeComponent},
   {path : 'login',component: LoginComponent},
-  {path : 'signup' ,component: SignupComponent}
+  {path : 'signup' ,component: SignupComponent},
+  {path :'admin',canActivate :[adminAuthGuard] ,loadChildren:()=>import('./admin/admin.module').then(module=>module.AdminModule)},
+  {
+    path :'courses',loadChildren :()=>import('./courses/courses.module').then(module=>module.CoursesModule)
+  }
 ];
 
 @NgModule({
