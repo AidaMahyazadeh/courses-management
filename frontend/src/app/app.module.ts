d8e67c0efd7285706e95cbcaa6dc3aa29fcc2ef3
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-09-06 10:31:32 +0200

    updated it after moving component in shared module

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -7,11 +7,11 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { SignupComponent } from './components/signup/signup.component';
+import { LoginComponent } from './shared/components/login/login.component';
+import { SignupComponent } from './shared/components/signup/signup.component';
 import { CoreModule } from './core/core.module';
 import { HeaderComponent } from './core/header/header.component';
-import { HomeComponent } from './components/home/home.component';
+import { HomeComponent } from './shared/components/home/home.component';
 
 
 
