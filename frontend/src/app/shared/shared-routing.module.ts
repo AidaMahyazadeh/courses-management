a25fc45e0c06120a08f36ac83b433300ee0e558a
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-09-06 17:39:11 +0200

    added some routes.

---
+++ frontend/src/app/shared/shared-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { HomeComponent } from "./components/home/home.component";
+import { LoginComponent } from "./components/login/login.component";
+import { SignupComponent } from "./components/signup/signup.component";
+import { CoursesRoutingModule } from "../courses/courses-routing.module";
+
+const routes :Routes =[
+    {path : 'home', component: HomeComponent},
+    {path : 'login',component: LoginComponent},
+    {path : 'signup' ,component: SignupComponent},
+]
+
+@NgModule({
+    imports : [
+        RouterModule.forChild(routes),
+        CoursesRoutingModule
+    ],
+    exports : [RouterModule]
+})
+export class SharedRoutingModule{}
