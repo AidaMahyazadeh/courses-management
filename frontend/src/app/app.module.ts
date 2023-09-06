206d4ca455551e1e3114a974349f1650fcebe81c
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-09-06 11:25:34 +0200

    imported sharedModule.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,35 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
-import { ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule} from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
+import { CoreModule } from './core/core.module';
 
 import { AppComponent } from './app.component';
-import { LoginComponent } from './shared/components/login/login.component';
-import { SignupComponent } from './shared/components/signup/signup.component';
-import { CoreModule } from './core/core.module';
-import { HeaderComponent } from './core/header/header.component';
-import { HomeComponent } from './shared/components/home/home.component';
+import { SharedModule } from './shared/shared.module';
+
+
 
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    SignupComponent,
-    HomeComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
     ToastrModule.forRoot(),
-    CoreModule
+    CoreModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
