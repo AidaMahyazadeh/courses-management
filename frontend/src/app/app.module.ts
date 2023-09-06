94b69cbbb8c55e994c63e35ee031c41bb48e5618
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-09-06 16:01:19 +0200

    deleted courseModule.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,10 +5,13 @@ import { HttpClientModule} from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 import { CoreModule } from './core/core.module';
+import { SharedModule } from './shared/shared.module';
 
 import { AppComponent } from './app.component';
-import { ReactiveFormsModule } from '@angular/forms';
-import { SharedModule } from './shared/shared.module';
+import { CoursesModule } from './courses/courses.module';
+
+
+
 
 
 
@@ -24,7 +27,8 @@ import { SharedModule } from './shared/shared.module';
     BrowserAnimationsModule,
     ToastrModule.forRoot(),
     CoreModule,
-    SharedModule
+    SharedModule,
+    CoursesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
