792ac757553a757fa1b2eada291f84b866d51a72
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-09-07 13:48:16 +0200

    declared courseDetails Component.

--- frontend/src/app/courses/courses.module.ts
+++ frontend/src/app/courses/courses.module.ts
@@ -4,13 +4,16 @@ import { CoursesComponent } from './courses.component';
 import { CoursesRoutingModule } from './courses-routing.module';
 import { SearchComponent } from './search/search.component';
 import { FormsModule } from '@angular/forms';
+import { CourseDetailsComponent } from './course-details/course-details.component';
 
 
 
 @NgModule({
   declarations: [
     CoursesComponent,
-    SearchComponent
+    SearchComponent,
+    CourseDetailsComponent,
+    
   ],
   imports: [
     CommonModule,
