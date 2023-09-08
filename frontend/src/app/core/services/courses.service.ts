190d14f2917bec9e63e8c0b7bb977891d4bc5c6b
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-09-08 18:48:52 +0200

    get single course by id.

--- frontend/src/app/core/services/courses.service.ts
+++ frontend/src/app/core/services/courses.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable, map } from 'rxjs';
 import ICourse from '../models/course.model';
 
 @Injectable({
@@ -8,11 +8,24 @@ import ICourse from '../models/course.model';
 })
 export class CoursesService {
  coursesUrl = '../assets/data/courses.data.json';
+ courses !:ICourse[];
+ courseSubject$ = new BehaviorSubject<ICourse[]>([]);
 
   constructor(private http :HttpClient) { }
 
   getAllCourses():Observable<ICourse[]>{
    return this.http.get<
    ICourse[]>(this.coursesUrl)
+  }
+
+  addCourse(course :ICourse){
+   this.courses.push(course)
+   this.courseSubject$.next(this.courses)
+  }
+
+  getCourseById(id:number):Observable<ICourse[]>{
+   return this.getAllCourses().pipe(
+     map(courses =>courses.filter(course=>course.id==id) )
+    )
   }
 }
