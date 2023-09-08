25edcdee50fa83af16505ffeb2c12c55e3399a9c
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-09-08 10:59:35 +0200

    imported NgbModalModule.

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -6,11 +6,9 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ToastrModule } from 'ngx-toastr';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { NgbModalModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { AppComponent } from './app.component';
-import { RouterModule } from '@angular/router';
-
-
 
 
 @NgModule({
@@ -24,8 +22,9 @@ import { RouterModule } from '@angular/router';
     BrowserAnimationsModule,
     ToastrModule.forRoot(),
     CoreModule,
+    NgbModalModule,
     SharedModule,
-   
+    
   
   ],
   providers: [],
