f632e43a9d46e46990a960e727373d6c3b008ce4
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-09-08 18:34:54 +0200

    removed payment route.

--- frontend/src/app/shared/shared-routing.module.ts
+++ frontend/src/app/shared/shared-routing.module.ts
@@ -5,10 +5,11 @@ import { LoginComponent } from "./components/login/login.component";
 import { SignupComponent } from "./components/signup/signup.component";
 import { CoursesRoutingModule } from "../courses/courses-routing.module";
 
+
 const routes :Routes =[
     {path : 'home', component: HomeComponent},
     {path : 'login',component: LoginComponent},
-    {path : 'signup' ,component: SignupComponent},
+    {path : 'signup' ,component: SignupComponent}
 ]
 
 @NgModule({
