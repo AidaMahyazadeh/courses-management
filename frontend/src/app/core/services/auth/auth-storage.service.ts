13e7cffe5a68531727318e254e9956e4a8306530
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-09-08 18:39:18 +0200

    stored some information in storage.

--- frontend/src/app/core/services/auth/auth-storage.service.ts
+++ frontend/src/app/core/services/auth/auth-storage.service.ts
@@ -1,9 +1,11 @@
 import { Injectable } from '@angular/core';
+import ICourse from '../../models/course.model';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthStorageService {
+ courses :ICourse[]=[];
 
   constructor() { }
 
@@ -23,10 +25,11 @@ export class AuthStorageService {
    return localStorage.getItem('role')
   }
 
-  removeTokenRole(){
+  removeUnneccessaryStorage(){
     localStorage.removeItem('token')
     localStorage.removeItem('role')
     localStorage.removeItem('username')
+    localStorage.removeItem('__paypal_storage__')
   }
 
   storeUserName(username :string){
@@ -35,5 +38,42 @@ export class AuthStorageService {
 
   getUserName(){
     return localStorage.getItem('username')
+  }
+
+  storeCartItem(course :ICourse[]){
+    localStorage.setItem('cartItem',JSON.stringify(course))
+  }
+
+  getCartItem():ICourse[]{
+    return this.courses = JSON.parse(localStorage.getItem('cartItem')!);
+  }
+
+  existedCourse(course:ICourse):boolean{
+    return( this.courses.findIndex(item=>item.id==course.id)>-1)
+  }
+
+  removeCourseFromCartItem(courseId:number){
+   this.courses = this.courses.filter(course=>course.id===courseId)
+    this.storeCartItem(this.courses)
+  }
+
+  removeCartItem(){
+    localStorage.removeItem('cartItem')
+  }
+
+  storeCartTotal(total :number){
+    localStorage.setItem('cartTotal',JSON.stringify(total.toFixed(2)))
+  }
+
+  getCartTotal(){
+    return JSON.parse(localStorage.getItem('cartTotal')!)
+  }
+
+  removeCartTotal(){
+    localStorage.removeItem('cartTotal')
+  }
+
+  clearPayPalStorage(){
+    localStorage.removeItem('__paypal_storage__')
   }
 }
