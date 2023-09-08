f1747794b6198e450b70699684dface10dee3fa2
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-09-08 18:37:52 +0200

    showing courses based of search input.

--- frontend/src/app/courses/courses.component.ts
+++ frontend/src/app/courses/courses.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CoursesService } from '../core/services/courses.service';
 import ICourse from '../core/models/course.model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-courses',
@@ -10,20 +11,29 @@ import ICourse from '../core/models/course.model';
 export class CoursesComponent implements OnInit{
   courses !:ICourse[];
   searchedCourse !:string;
+  
 
-  constructor(private coursesService:CoursesService){}
+  constructor(
+    private coursesService:CoursesService,
+    private router:Router
+    ){}
 
   ngOnInit(){
     this.coursesService.getAllCourses().subscribe(
       res => this.courses=res
     )
+    
   }
 
   onSearchedCourse(searchText :string){
    this.searchedCourse=searchText
   }
 
-  addItemToCart(){
-    
+  getRoute(){
+    let showAllCourses = true;
+    if(this.router.url.endsWith('courses')){
+      return showAllCourses= true
+    }
+    return showAllCourses=false
   }
 }
