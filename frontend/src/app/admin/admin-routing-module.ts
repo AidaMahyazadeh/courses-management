72ddc6935716cb460a8e09ef72f62981b3dcabee
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-09-08 18:46:49 +0200

    added userlist route.

--- frontend/src/app/admin/admin-routing-module.ts
+++ frontend/src/app/admin/admin-routing-module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { AdminComponent } from "./admin.component";
+import { UserslistComponent } from "./userslist/userslist.component";
 
 const routes :Routes =[
-    {path :'',component:AdminComponent,pathMatch:'full'}
+    {path :'',component:AdminComponent,pathMatch:'full',children:[
+        {path :'user-list',component :UserslistComponent}
+    ]}
 ]
     
 
