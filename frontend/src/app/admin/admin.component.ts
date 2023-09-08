97c2f970813458735ecd9b7aae78d707e94c1030
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-09-08 18:49:52 +0200

    show admin 's name.

--- frontend/src/app/admin/admin.component.ts
+++ frontend/src/app/admin/admin.component.ts
@@ -1,10 +1,27 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { AuthStorageService } from "../core/services/auth/auth-storage.service";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
     selector: 'app-admin',
     templateUrl: './admin.component.html',
     styleUrls: ['./admin.component.css']
   })
-export class AdminComponent{
+export class AdminComponent implements OnInit{
+adminName !:string;
+showContent =false;
+
+constructor(
+  private authStorageService :AuthStorageService,
+  private router :Router,
+  private activatedRout :ActivatedRoute
+  ){}
+
+   ngOnInit(){
+    this.adminName = (this.authStorageService.getUserName()!)
+   
+   }
+
+  
 
 }
