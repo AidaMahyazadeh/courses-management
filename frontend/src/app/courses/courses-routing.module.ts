2e76fcc0759190e929d32a754151b4faf14a38c4
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-09-08 18:48:02 +0200

    added route by name & courseId.

--- frontend/src/app/courses/courses-routing.module.ts
+++ frontend/src/app/courses/courses-routing.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { CoursesComponent } from "./courses.component";
+import { CourseDetailsComponent } from "./course-details/course-details.component";
 
 
 const routes :Routes =[
-    {path:'',component :CoursesComponent,pathMatch:'full'}
+    {path:'',component :CoursesComponent,children:[
+      {path :':courseName/:id',component :CourseDetailsComponent}
+    ]}
 ]
 
 @NgModule({
