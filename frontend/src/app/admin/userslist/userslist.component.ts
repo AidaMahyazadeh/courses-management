2b02745313b099d94bdf9b8bac497d9aacb33ced
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-09-13 13:58:43 +0200

    get userList from localStorage & show it.

--- frontend/src/app/admin/userslist/userslist.component.ts
+++ frontend/src/app/admin/userslist/userslist.component.ts
@@ -1,23 +1,33 @@
 import { Component, OnInit } from '@angular/core';
-import { AuthenticationService } from 'src/app/core/services/auth/authentication.service';
+import { Subscription } from 'rxjs';
+import ICourse from 'src/app/core/models/course.model';
+import IUser from 'src/app/core/models/user.model';
+import { AdminAuthStorageService } from 'src/app/core/services/admin/admin-auth-storage.service';
+import { UsersListService } from 'src/app/core/services/admin/users-list.service';
+
+
 
 @Component({
   selector: 'app-userslist',
   templateUrl: './userslist.component.html',
   styleUrls: ['./userslist.component.css']
 })
-export class UserslistComponent implements OnInit {
-
-  constructor(private authenticationService:AuthenticationService){}
+export class UserslistComponent implements OnInit{
+  users !:IUser[];
+  subscription !:Subscription;
+  constructor(private adminAuthStorageService :AdminAuthStorageService){}
 
   ngOnInit(){
-    this.authenticationService.getAllUsers().subscribe({
-      next : (res)=>{
- 
-      },
-      error :(err)=>{
+  this.users=this.adminAuthStorageService.getAllUsers()  
+}
 
-      }
-    })
-  }
+// insertEnrolledCourse(username:string,courses:ICourse[]){
+//  let user= this.users.find(user=>user.username==username)
+//  user?.enrolledCourse?.push(...courses)
+// }
+
+onDelete(username:string){
+  this.users = this.users.filter(user=>user.username!=username)
+}
+
 }
