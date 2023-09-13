d38e64f39ce9b32495a81250933c45b597d6a87e
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-09-13 13:53:30 +0200

    canceled extra spaces.

--- frontend/src/app/core/core.module.ts
+++ frontend/src/app/core/core.module.ts
@@ -20,11 +20,11 @@ import { FooterComponent } from './footer/footer.component';
   ],
   exports :[HeaderComponent,FooterComponent],
   providers :[
-    // {
-    //   provide : HTTP_INTERCEPTORS,
-    //   useClass :AuthInterceptor,
-    //   multi :true
-    // }
+    {
+      provide : HTTP_INTERCEPTORS,
+      useClass :AuthInterceptor,
+      multi :true
+    }
   ]
 })
 export class CoreModule { }
