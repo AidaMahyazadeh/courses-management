d1f75d2e790b19f8c42c94df3189e3ec22f1711c
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-09-14 15:20:30 +0200

    imported ReactiveFormModule.

--- frontend/src/app/admin/admin.module.ts
+++ frontend/src/app/admin/admin.module.ts
@@ -6,7 +6,9 @@ import { SidebarComponent } from './sidebar/sidebar.component';
 import { UserslistComponent } from './userslist/userslist.component';
 import { CourseListComponent } from './course-list/course-list.component';
 import { ProfessorsComponent } from './professors/professors.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AddProfessorComponent } from './add-professor/add-professor.component';
+import { ModifyModalComponent } from './modify-modal/modify-modal.component';
 
 
 
@@ -17,12 +19,14 @@ import { FormsModule } from '@angular/forms';
     SidebarComponent,
     UserslistComponent,
     CourseListComponent,
-    ProfessorsComponent
+    ProfessorsComponent,
+    AddProfessorComponent,
+    ModifyModalComponent
   ],
   imports: [
     CommonModule,
     AdminRoutingModule,
-    FormsModule
+    ReactiveFormsModule
   ]
 })
 export class AdminModule { }
