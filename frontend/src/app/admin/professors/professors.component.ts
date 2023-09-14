9fc5370fd6be4006f0df7a927acbbf483331af92
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-09-14 15:25:13 +0200

    return back edtited detail  or new professor.

--- frontend/src/app/admin/professors/professors.component.ts
+++ frontend/src/app/admin/professors/professors.component.ts
@@ -1,6 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { IProfessor } from 'src/app/core/models/professor.model';
 import { ProfessorService } from 'src/app/core/services/admin/professor.service';
+import { AddProfessorComponent } from '../add-professor/add-professor.component';
+import { ModifyModalComponent } from '../modify-modal/modify-modal.component';
 
 @Component({
   selector: 'app-professors',
@@ -9,8 +12,14 @@ import { ProfessorService } from 'src/app/core/services/admin/professor.service'
 })
 export class ProfessorsComponent implements OnInit {
 professores !:IProfessor[];
+newaddedprofessor !:IProfessor;
+selectedProfessor !:IProfessor;
 
-constructor(private professorsService:ProfessorService){}
+
+constructor(
+  private professorsService:ProfessorService,
+  private modalService :NgbModal
+  ){}
 
 ngOnInit() {
   this.professorsService.getAllProfessore().subscribe(
@@ -18,7 +27,30 @@ ngOnInit() {
   )
 }
 
-onDelete(id :number){
+openModal(){
+ const modalRef= this.modalService.open(AddProfessorComponent,{size:'lg'})
+ modalRef.componentInstance.newProfessor=this.newaddedprofessor;
+ modalRef.componentInstance.newAddedProfessor.subscribe((res :IProfessor)=>{
+ this.professores.push(res)
+ }
+ )
+
+}
+
+getProfessorById(professorName :string):IProfessor{
+ return this.selectedProfessor =this.professores.find(professor=>professor.name ==professorName)!
+}
+
+openModify(name:string){
+this.getProfessorById(name)
+const modalRef= this.modalService.open(ModifyModalComponent,{size:'lg'})
+modalRef.componentInstance.selectedProfessor=this.selectedProfessor
+
+}
+
+
+
+onDeleteProfessor(id :number){
   this.professores=this.professores.filter(professor=>professor.id!=id)
 }
 }
