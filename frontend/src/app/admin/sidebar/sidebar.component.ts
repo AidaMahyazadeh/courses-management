1289badc3884feb550e7913172e84097b3a6f6bd
aida mahyazadeh <aidamahyazadeh83@gmail.com>
2023-09-14 15:24:18 +0200

    get admin name from localStorage.

--- frontend/src/app/admin/sidebar/sidebar.component.ts
+++ frontend/src/app/admin/sidebar/sidebar.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AuthStorageService } from 'src/app/core/services/auth/auth-storage.service';
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
-export class SidebarComponent {
+export class SidebarComponent implements OnInit {
+  adminName:string ='';
+  constructor(private authStorageService : AuthStorageService){}
+
+  ngOnInit(): void {
+    this.adminName= this.authStorageService.getUserName()
+  }
 
 }
